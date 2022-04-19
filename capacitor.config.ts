f46645143ccaeeb7b40d88ca2a61b5694e8b3e19
bastiannse <92299464+bastiannse@users.noreply.github.com>
2022-04-19 14:23:11 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UniMeal',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
